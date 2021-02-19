f930fff14b5834e953f08dc6fa0bc9bc2a444d72
gingivitis <e0203297@u.nus.edu>
2021-02-19 18:55:36 +0800

    added basic body rotation with headset

--- src/VRtigo/Assets/Scripts/Movement.cs
+++ src/VRtigo/Assets/Scripts/Movement.cs
@@ -5,27 +5,43 @@ using Valve.VR;
 
 public class Movement : MonoBehaviour
 {
-    // a reference to the action
-    public SteamVR_Action_Vector2 movementDirection;
+    // vector taken from joystick movement
+    public SteamVR_Action_Vector2 movementDirInput;
+
     // a reference to the hand
     public SteamVR_Input_Sources handType;
-    public float movementSpeed = 0.03f;
+
+    public float movementSpeed = 3.0f;
+    public GameObject headset;
+    public Rigidbody playerRigidbody;
+
+    // processed vr inputs
+    public Vector2 movementDir;
 
     void Start()
     {
-        movementDirection.AddOnAxisListener(Move, handType);
+        movementDirInput.AddOnAxisListener(ProcessMovementDirInput, handType);
+        playerRigidbody = this.GetComponent<Rigidbody>();
     }
 
-    void update() 
-    {
+    private void FixedUpdate() {
+        Vector3 playerMovementDir = headset.transform.forward * movementDir.y + headset.transform.right * movementDir.x;
+        playerMovementDir.y = 0;
+        playerMovementDir.Normalize();
 
+        //Debug.Log(playerMovementDir.sqrMagnitude);
+
+        if (movementDir.sqrMagnitude > 0.05f) {
+            playerRigidbody.velocity = movementSpeed * playerMovementDir;
+        }
+        else {
+            playerRigidbody.velocity = Vector3.zero;
+        }
     }
 
     // Handles vector input from right joystick
-    public void Move(SteamVR_Action_Vector2 fromAction, SteamVR_Input_Sources fromSource, Vector2 axis, Vector2 delta) {
-        //Axis gives direction
-        Debug.Log("Axis: " + axis + ", Delta: " + delta);
-        Vector3 v = new Vector3(axis.x, 0, axis.y);
-        transform.Translate(v * movementSpeed);
+    public void ProcessMovementDirInput(SteamVR_Action_Vector2 fromAction, SteamVR_Input_Sources fromSource, Vector2 axis, Vector2 delta) 
+    {
+        movementDir = axis;
     }
 }
