4c4a9f68f70cecdb79b5f6f767b020e909ce3fdc
bannified <freedomzelda_dc@hotmail.com>
2021-02-23 19:23:25 +0800

    Add backward movement for default Player Initiated Movement

--- src/VRtigo/Assets/Scripts/PlayerInitiatedMovement/Character_PlayerInitiatedMovement.cs
+++ src/VRtigo/Assets/Scripts/PlayerInitiatedMovement/Character_PlayerInitiatedMovement.cs
@@ -130,7 +130,22 @@ public class Character_PlayerInitiatedMovement : Character
         {
             if (m_InputAxisValue > m_LinearMovementInputThreshold)
             {
-                m_Rigidbody.velocity = m_MaxMoveSpeed * resultMoveDirection;
+                if (HasFlagsEnabled(PlayerInitiatedMovementBitmask.LateralMovementEnabled))
+                {
+                    m_Rigidbody.velocity = m_MaxMoveSpeed * resultMoveDirection;
+                } else
+                { // standard movement
+                    if (m_InputDirection.y > -0.3f)
+                    {
+                        // move forward
+                        m_Rigidbody.velocity = m_MaxMoveSpeed * resultMoveDirection;
+                    }
+                    else
+                    {
+                        // move backward
+                        m_Rigidbody.velocity = m_MaxMoveSpeed * -resultMoveDirection;
+                    }
+                }
             } else
             {
                 m_Rigidbody.velocity = Vector3.zero;
