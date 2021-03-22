f83c3945459358f3acc27eef3290d28f7cbaeaff
bannified <freedomzelda_dc@hotmail.com>
2021-03-22 11:57:21 +0800

    Fix Classroom ending prematurely at the final lesson step Make subtitle reveal all at once Add the notion of Lesson Length and Post Lesson auto skip duration

--- src/VRtigo/Assets/Scripts/Classroom/ClassroomScreen.cs
+++ src/VRtigo/Assets/Scripts/Classroom/ClassroomScreen.cs
@@ -9,14 +9,22 @@ public class ClassroomScreen : MonoBehaviour
     [SerializeField]
     private TMP_Text m_SubtitleText;
 
-    // Subtitle Reveal Speed in characters per second
-    [SerializeField]
-    private float m_SubtitleRevealSpeed = 10.0f;
-
     private Coroutine m_DialogueRevealCoroutine;
+    private Coroutine m_PostLessonCoroutine;
 
     [SerializeField]
     private ClassroomNarrator m_Narrator;
+
+    [SerializeField, 
+        Tooltip("The length of the lesson in seconds. After this duration, the lesson will skip to the End of the Lesson.\n" +
+        "If this value is negative, the lesson will go to the end of the lesson automatically.")]
+    private float m_LessonLength = 20.0f;
+
+    [SerializeField,
+    Tooltip("The duration to be elapsed post LessonLength in seconds, before the lesson goes to the next step. " +
+        "After this duration, the lesson will skip to the next step.\n" +
+    "If this value is negative, the lesson will not skip forward to the next step automatically.")]
+    private float m_PostLessonEndAutoSkipDelay = -1.0f;
 
     private void Awake()
     {
@@ -40,6 +48,11 @@ public class ClassroomScreen : MonoBehaviour
             StopCoroutine(m_DialogueRevealCoroutine);
         }
 
+        if (m_PostLessonCoroutine != null)
+        {
+            StopCoroutine(m_PostLessonCoroutine);
+        }
+
         m_DialogueRevealCoroutine = StartCoroutine(DialogueRevealRoutine());
 
         m_Narrator.NarrateString(m_SubtitleText.text);
@@ -47,9 +60,9 @@ public class ClassroomScreen : MonoBehaviour
 
     private void HandleOnLessonStepEnd(LessonStep lessonStep)
     {
-        m_DialogueRevealCoroutine = null;
-
         RevealWholeSubtitle();
+
+        m_PostLessonCoroutine = StartCoroutine(PostLessonRoutine());
     }
 
     private void HandleOnLessonStepSkip(LessonStep lessonStep)
@@ -64,20 +77,31 @@ public class ClassroomScreen : MonoBehaviour
 
     private void RevealWholeSubtitle()
     {
-        m_SubtitleText.maxVisibleCharacters = m_SubtitleText.GetParsedText().Length;
+        m_SubtitleText.maxVisibleCharacters = m_SubtitleText.text.Length;
     }
 
     private IEnumerator DialogueRevealRoutine()
     {
-        int parsedTextLength = m_SubtitleText.text.Length;
-        while (m_SubtitleText.maxVisibleCharacters < parsedTextLength)
-        {
-            ++m_SubtitleText.maxVisibleCharacters;
+        RevealWholeSubtitle();
 
-            yield return new WaitForSeconds(1.0f / m_SubtitleRevealSpeed);
+        if (m_LessonLength >= 0.0f)
+        {
+            yield return new WaitForSeconds(m_LessonLength);
         }
 
         ClassroomManager.Instance.EndLessonStep();
+
+        yield return null;
+    }
+
+    private IEnumerator PostLessonRoutine()
+    {
+        if (m_PostLessonEndAutoSkipDelay >= 0.0f)
+        {
+            yield return new WaitForSeconds(m_PostLessonEndAutoSkipDelay);
+            ClassroomManager.Instance.NextStep();
+        }
+
         yield return null;
     }
 
