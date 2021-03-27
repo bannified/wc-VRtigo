66a28991e5c3d47524337515c237087e96eaf4b7
Dominic Chong <freedomzelda_dc@hotmail.com>
2021-03-28 01:20:18 +0800

    Dom/playtest candidate (#81)
    * Small adjustments to Classroom elements
    
    * Add actual Lesson Data for PlayerInitiatedMovement
    
    * Balance changes to non-linear movement and Turning
    
    * Undo changes to defaults for Player Initiated Movement

--- src/VRtigo/Assets/Scripts/Activatables/Player/PlayerSpeedChangeActivatable.cs
+++ src/VRtigo/Assets/Scripts/Activatables/Player/PlayerSpeedChangeActivatable.cs
@@ -7,12 +7,16 @@ public class PlayerSpeedChangeActivatable : MonoBehaviour, IActivatable
     [SerializeField]
     private float m_SpeedChangeScale = 1.0f;
 
+    [SerializeField]
+    private float m_AccelScale = 1.0f;
+
     public void Activate()
     {
         Character_PlayerInitiatedMovement character = GameManager.Instance.GetCharacter(0) as Character_PlayerInitiatedMovement;
         if (character != null)
         {
             character.SetMaxMoveSpeed(character.GetMaxMoveSpeed() * m_SpeedChangeScale);
+            character.SetAccelScale(m_AccelScale);
         }
     }
 }
