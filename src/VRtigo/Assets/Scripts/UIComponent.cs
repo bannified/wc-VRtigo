ddf235ff9ba0623016cfa91bf991b60451a5f6d3
Anthony Halim <anthony.halim@live.com>
2021-03-30 12:45:18 +0800

    Update comment

--- src/VRtigo/Assets/Scripts/UIComponent.cs
+++ src/VRtigo/Assets/Scripts/UIComponent.cs
@@ -6,7 +6,7 @@ public class UIComponent : MonoBehaviour
 {
     /**
      * Set the UI component to be visible or invisible. UI component
-     * should perform their necessary transition (fade in/fade out) here.
+     * should perform their necessary transition (e.g. fade in/fade out) here.
      */
     public virtual void SetVisible() { }
 
@@ -18,6 +18,9 @@ public class UIComponent : MonoBehaviour
      *
      * Typically used after player trigger an event/flag, where some UI component
      * will be enabled or disabled.
+     * 
+     * UIComponent should handle the enabled/disabled gracefully i.e. call their
+     * transition accordingly.
      *
      * Use case: After player finished the lesson, the 'Continue' UI component
      * of the continue button should no longer appear.
